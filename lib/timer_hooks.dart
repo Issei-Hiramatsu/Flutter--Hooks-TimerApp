28d4a7324b9f57e0617ab974bb038e0f5cff5320
issei <issei.hiramatsu@gmail.com>
2023-01-26 10:38:20 +0900

    Fix: hooks_riverpod to flutter_hooks

---
+++ lib/timer_hooks.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class InfiniteTimer extends HookConsumerWidget {
+  const InfiniteTimer({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Container();
+  }
+}
